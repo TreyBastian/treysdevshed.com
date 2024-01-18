ec962328a4fea1de38d8683436e2393831734b57
Trey Bastian <2991824+TreyBastian@users.noreply.github.com>
2024-01-18 11:53:36 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,41 @@
+const options = {
+    channel: "treysdevshed",
+    height: 576,
+    width: '100%',
+    layout: 'video',
+    parent: ['treysdevshed.com']
+};
+const twitchPlayer = new Twitch.Embed("twitch-video", options);
+
+
+const initiate = () => {
+    twitchPlayer.addEventListener(Twitch.Player.ONLINE, handleOnline);
+    twitchPlayer.addEventListener(Twitch.Player.OFFLINE, handleOffline);
+    twitchPlayer.removeEventListener(Twitch.Player.READY, initiate);
+};
+
+const handleOnline = () => {
+    setTimeout(() => {
+        document.getElementById("twitch-video").classList.remove('hide');
+        document.getElementById("twitch-video").classList.add('show');
+    }, 600);
+    document.getElementById("header-image").classList.add('hide');
+    document.getElementById("header-image").classList.remove('show');
+    twitchPlayer.removeEventListener(Twitch.Player.ONLINE, handleOnline);
+    twitchPlayer.addEventListener(Twitch.Player.OFFLINE, handleOffline);
+    twitchPlayer.setMuted(false);
+}
+
+const handleOffline = () => {
+    setTimeout(() => {
+        document.getElementById("header-image").classList.remove('hide');
+        document.getElementById("header-image").classList.add('show');
+    }, 600);
+    document.getElementById("twitch-video").classList.add('hide');
+    document.getElementById("twitch-video").classList.remove('show');
+    twitchPlayer.removeEventListener(Twitch.Player.OFFLINE, handleOffline);
+    twitchPlayer.addEventListener(Twitch.Player.ONLINE, handleOnline);
+    twitchPlayer.setMuted(true);
+}
+
+twitchPlayer.addEventListener(Twitch.Player.READY, initiate)
